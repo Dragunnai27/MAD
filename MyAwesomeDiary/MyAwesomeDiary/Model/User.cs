13fc9eb88d36961a807445134ff187d24306f3d4
khoa0319@gmail.com <khoa0319@gmail.com>
2018-05-08 23:25:42 +0700

    update user table and EntityFramework, SQL compact

---
+++ MyAwesomeDiary/MyAwesomeDiary/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAwesomeDiary.Model
+{
+    public class User
+    {
+        [Key]
+        public string UserId { get; set; }
+        [Required]
+        public string Pass { get; set; }
+        [StringLength(20, ErrorMessage = "invalid int",MinimumLength = 5)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age { get; set; }
+    }
+}
